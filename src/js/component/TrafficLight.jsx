09366a3b60a08f0deb18cbd12875e0f655997617
ArnauOliveras <77742762+ArnauOliveras@users.noreply.github.com>
2024-12-11 20:33:25 +0100

    commit2

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 
 //create your first component
 const TrafficLight = () => {
 	const [color, setColor] = useState("red");
 
-	useEffect(() => {
-		if (color === "red") {
-			document.getElementById("red").classList.add("encendida");
-			document.getElementById("yellow").classList.remove("encendida");
-			document.getElementById("green").classList.remove("encendida");
-		}
-		if (color === "yellow") {
-			document.getElementById("yellow").classList.add("encendida");
-			document.getElementById("red").classList.remove("encendida");
-			document.getElementById("green").classList.remove("encendida");
-		}
-		if (color === "green") {
-			document.getElementById("green").classList.add("encendida");
-			document.getElementById("yellow").classList.remove("encendida");
-			document.getElementById("red").classList.remove("encendida");
-		}
-	}, [color]);
-
-
 	return (
 		<div class="semaforo ">
-			<button onClick={() => setColor("red")} class="luz roja" id="red"></button>
-			<button onClick={() => setColor("yellow")} class="luz amarilla" id="yellow"></button>
-			<button onClick={() => setColor("green")} class="luz verde" id="green"></button>
+			<button onClick={() => setColor("red")} class={`luz roja ${color == "red" ? "encendida" : ""}`}></button>
+			<button onClick={() => setColor("yellow")} class={`luz amarilla ${color == "yellow" ? "encendida" : ""}`}></button>
+			<button onClick={() => setColor("green")} class={`luz verde ${color == "green" ? "encendida" : ""}`}></button>
 		</div>
 	);
 };
